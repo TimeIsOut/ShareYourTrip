3faf7df95fd53e90764f1916287cb55148aba726
TimeIsOut <y0u-g0t-hack3d@yandex.ru>
2021-12-22 12:22:16 +0300

    fix: change articles database

--- data/articles.py
+++ data/articles.py
@@ -1,14 +1,13 @@
 import sqlalchemy as sa
 from .db_session import SqlAlchemyBase
-from datetime import utcnow
 
 
 class Article(SqlAlchemyBase):
-    id = sa.Column(sa.Integer, primary_key=True)
-    title = sa.Column(sa.String(100), nullable=False)
+    __tablename__ = 'articles'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title = sa.Column(sa.String(50), nullable=False)
     intro = sa.Column(sa.String(300), nullable=False)
     text = sa.Column(sa.Text, nullable=False)
-    date = sa.Column(sa.DateTime, default=utcnow)
 
     def __repr__ (self):
-        return '<Article %r>' % self.id
+        return f'<Article {self.id}>'
